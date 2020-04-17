91e78da2d5722b292c82ab71f506ffb6f949f5e1
Nick Garcia <NicksIdeaEngine@gmail.com>
2020-04-17 12:06:45 -0400

    Unsuccessful attempt at using tables for sudoku board

--- src/lib/buildBoard.js
+++ src/lib/buildBoard.js
@@ -1,67 +1,65 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
+import React from 'react';
+
 function buildBoard(maxRows = 9, maxColumns = 9, regionSize = 3) {
   const newBoard = [];
+  let newRowContents = [];
   let id = 0;
   const value = '';
   let row = 1;
   let column = 1;
   let region = 1;
-  let classNames = [];
-  let style = {};
-  let offset = 0;
+  let className = [];
   const candidates = [];
 
   for (row = 1; row <= maxRows; row += 1) {
+    newRowContents = [];
     for (column = 1; column <= maxColumns; column += 1) {
-      classNames = [];
-      offset = 0;
-      style = { left: '', top: '' };
-      classNames.push('sudoku-tile');
-      if (column === 1) {
-        classNames.push('sudoku-tile-outer-left');
-      } else {
-        offset = (column - 1) * -0.2;
-        style.left = `${offset}em`;
-      }
-      if (column === 9) classNames.push('sudoku-tile-outer-right');
-      if (row === 1) {
-        classNames.push('sudoku-tile-outer-top');
-      } else {
-        offset = (row - 1) * -0.2;
-        style.top = `${offset}em`;
-      }
-      if (row === 9) classNames.push('sudoku-tile-outer-bottom');
-      if (column === 3 || column === 6) {
-        classNames.push('sudoku-tile-inner-right');
-      }
-      if (column === 4 || column === 7) {
-        classNames.push('sudoku-tile-inner-left');
-      }
-      if (row === 3 || row === 6) {
-        classNames.push('sudoku-tile-inner-bottom');
-      }
-      if (row === 4 || row === 7) {
-        classNames.push('sudoku-tile-inner-top');
-      }
+      className = [];
+      className.push('sudoku-td');
+      // if (column === 1) {
+      //   className.push('sudoku-tile-outer-left');
+      // }
+      // if (column === 9) className.push('sudoku-tile-outer-right');
+      // if (row === 1) {
+      //   className.push('sudoku-tile-outer-top');
+      // }
+      // if (row === 9) className.push('sudoku-tile-outer-bottom');
+      // if (column === 3 || column === 6) {
+      //   className.push('sudoku-tile-inner-right');
+      // }
+      // if (column === 4 || column === 7) {
+      //   className.push('sudoku-tile-inner-left');
+      // }
+      // if (row === 3 || row === 6) {
+      //   className.push('sudoku-tile-inner-bottom');
+      // }
+      // if (row === 4 || row === 7) {
+      //   className.push('sudoku-tile-inner-top');
+      // }
 
       region =
         Math.ceil(column / regionSize) +
         Math.floor((row - 1) / regionSize) * regionSize;
 
-      newBoard.push({
+      newRowContents.push({
         id,
         row,
         column,
         region,
-        classNames: classNames.join(' '),
-        style,
+        className: className.join(' '),
         candidates,
         value,
       });
       id += 1;
     }
+    newBoard.push(
+      <tr className="sudoku-tr" id={`row-${row - 1}`}>
+        {newRowContents}
+      </tr>,
+    );
   }
-
   return newBoard;
 }
 
